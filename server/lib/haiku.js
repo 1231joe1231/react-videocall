eb59eb58c083fcce8ce8194990da41fc95eb7873
Joe <zhuangruixin1231@gmail.com>
2021-08-06 23:39:32 +0800

    Rework server id

--- server/lib/haiku.js
+++ server/lib/haiku.js
@@ -1,33 +1,36 @@
 const adjs = [
-  'autumn', 'hidden', 'bitter', 'misty', 'silent', 'empty', 'dry', 'dark',
-  'summer', 'icy', 'delicate', 'quiet', 'white', 'cool', 'spring', 'winter',
-  'patient', 'twilight', 'dawn', 'crimson', 'wispy', 'weathered', 'blue',
-  'billowing', 'broken', 'cold', 'damp', 'falling', 'frosty', 'green',
-  'long', 'late', 'lingering', 'bold', 'little', 'morning', 'muddy', 'old',
-  'red', 'rough', 'still', 'small', 'sparkling', 'throbbing', 'shy',
-  'wandering', 'withered', 'wild', 'black', 'young', 'holy', 'solitary',
-  'fragrant', 'aged', 'snowy', 'proud', 'floral', 'restless', 'divine',
-  'polished', 'ancient', 'purple', 'lively', 'nameless'
+  "可爱",
+  "粉红",
+  "开心",
+  "勤奋",
+  "快乐",
+  "幸福",
+  "深蓝",
+  "浅蓝",
+  "呆呆",
+  "傻傻",
+  "没头脑",
+  "不高兴",
+  "性感",
 ];
 
 const nouns = [
-  'waterfall', 'river', 'breeze', 'moon', 'rain', 'wind', 'sea', 'morning',
-  'snow', 'lake', 'sunset', 'pine', 'shadow', 'leaf', 'dawn', 'glitter',
-  'forest', 'hill', 'cloud', 'meadow', 'sun', 'glade', 'bird', 'brook',
-  'butterfly', 'bush', 'dew', 'dust', 'field', 'fire', 'flower', 'firefly',
-  'feather', 'grass', 'haze', 'mountain', 'night', 'pond', 'darkness',
-  'snowflake', 'silence', 'sound', 'sky', 'shape', 'surf', 'thunder',
-  'violet', 'water', 'wildflower', 'wave', 'water', 'resonance', 'sun',
-  'wood', 'dream', 'cherry', 'tree', 'fog', 'frost', 'voice', 'paper',
-  'frog', 'smoke', 'star'
+  "猪猪",
+  "狗狗",
+  "小鸟",
+  "兔兔",
+  "小朋友",
+  "小马",
+  "便便",
+  "丹丹",
 ];
 
 module.exports = () => {
   const adj = adjs[Math.floor(Math.random() * adjs.length)];
   const noun = nouns[Math.floor(Math.random() * nouns.length)];
-  const MIN = 1000;
-  const MAX = 9999;
-  const num = Math.floor(Math.random() * ((MAX + 1) - MIN)) + MIN;
+  const MIN = 10;
+  const MAX = 99;
+  const num = Math.floor(Math.random() * (MAX + 1 - MIN)) + MIN;
 
-  return `${adj}-${noun}-${num}`;
+  return `${adj}的${noun}${num}`;
 };
